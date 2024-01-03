38339d02c2840c99a0a648973ded61480964c46c
Shaymoun <enter312123@gmail.com>
2024-01-03 20:13:50 +0100

    feat:Added basic toggled burger menu.

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -5,17 +5,24 @@ import { EventifyContext } from "../store/eventify-context"
 
 const MainNavigation = () => {
 	const { darkMode, mobileMenuOpen, toggleMenu } = useContext(EventifyContext)
+
 	return (
-		<header>
-			<div className='logo'></div>
-			<NavigationButton mobileMenuOpen={mobileMenuOpen} toggleMenu={toggleMenu}/>
-			<nav>
-				<ul>
+		<header className='relative flex justify-between overflow-x-hidden bg-sky-500'>
+			<div>LOGO</div>
+			<nav
+				className={`bg-sky-500 fixed inset-0 h-dvh z-[-1] ${
+					mobileMenuOpen ? "" : "translate-x-full"
+				}`}>
+				<ul className='flex flex-col justify-around items-center h-full w-full'>
 					<NavLink to='/profile'>Profile</NavLink>
 					<NavLink to='/add-event'>Add Event</NavLink>
 					<NavLink to='/my-events'>My Events</NavLink>
 				</ul>
 			</nav>
+			<NavigationButton
+				mobileMenuOpen={mobileMenuOpen}
+				toggleMenu={toggleMenu}
+			/>
 		</header>
 	)
 }
--- src/ui/NavigationButton.jsx
+++ src/ui/NavigationButton.jsx
@@ -3,18 +3,17 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 
 const NavigationButton = ({ mobileMenuOpen, toggleMenu, ...props }) => {
 	let icon = mobileMenuOpen ? (
-		<XMarkIcon className='block h-6 w-6' />
+		<XMarkIcon className='block h-10 w-10' />
 	) : (
-		<Bars3Icon className='block h-6 w-6' />
+		<Bars3Icon className='block h-10 w-10' />
 	)
 
 	return (
 		<button
 			{...props}
 			aria-label={mobileMenuOpen ? "Close menu" : "Open menu"}
-			className=''
 			onClick={toggleMenu}
-			>
+			className='px-3 py-1.5'>
 			{icon}
 		</button>
 	)
