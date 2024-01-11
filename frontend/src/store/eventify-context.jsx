3a74e4d76c52b1b2e2cfcb324da52d19f541f0f5
Shaymoun <enter312123@gmail.com>
2024-01-11 22:26:10 +0100

    feat: Created user isLoggedIn state in context.

--- frontend/src/store/eventify-context.jsx
+++ frontend/src/store/eventify-context.jsx
@@ -1,14 +1,28 @@
 import { createContext, useState } from "react"
 
 export const EventifyContext = createContext({
+	isLoggedIn: false,
 	darkMode: false,
+	logIn: () => {},
+	logOut: () => {},
 })
 
 const EventifyContextProvider = ({ children }) => {
 	const [darkMode, setDarkMode] = useState(false)
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+	const logIn = () => {
+		setIsLoggedIn(true)
+	}
+	const logOut = () => {
+		setIsLoggedIn(false)
+	}
 
 	const ctxValue = {
 		darkMode,
+		isLoggedIn,
+		logIn,
+		logOut,
 	}
 
 	return (
